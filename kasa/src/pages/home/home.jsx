6005f23f3efc64baa55cf6cace1cdda51db9ca48
Petochh <petoche.pereira@gmail.com>
2024-04-18 18:44:42 +0200

    final

--- kasa/src/pages/home/home.jsx
+++ kasa/src/pages/home/home.jsx
@@ -7,23 +7,10 @@ import Galery from "../../components/homeGallery/gallery"
 import "./home.scss"
 
 function Home(){
-    const [text, setText] = useState("Chez vous, partout et ailleurs");
-
-    useEffect(() => {
-        const handleResize = () => {
-        if (window.innerWidth <= 768) {
-            setText(<p>Chez vous,<br/> partout et ailleurs</p>);
-        } else {
-            setText(<p>Chez vous, partout et ailleurs</p>);
-        }
-        };
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []); // Déclenche l'effet uniquement lors du montage initial
-
+    
     return (
         <div className="home_page_container">
-            <Banner image={HomeImg} text={text} alt="Banniere accueil" />
+            <Banner image={HomeImg} text="Chez vous, partout et ailleurs" alt="Banniere accueil" />
             <Galery />
         </div>
     );
